378731b085788b4e2d0eb3e1ee20b411b35cace5
annumudgal001 <akshitmudgal001@gmail.com>
2025-03-04 15:16:19 +0530

    separation of concerns

--- database/db.js
+++ database/db.js
@@ -1,13 +1,18 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();  // Add this line to load the environment variables
 
-const connecttodb= async ()=>{
-    try{
-        await mongoose.connect('mongodb+srv://mejohnybravo69:Annu1234.@cluster0.55umd.mongodb.net/')
+const connecttodb = async () => {
+    try {
+        // Use the MONGODB_URI from the .env file
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('Connected to MongoDB');
-    } catch (error){
-        console.error("Failed to connect to MongoDB:", error)
-        process.exit(1);  // Exit the application with an error status code. 1 signifies failure. 0 signifies success. 3 signifies command-line tool failure. 130 signifies unexpected system failure. 128 signifies signal termination. 129 signifies fatal error. 137 signifies segmentation fault. 139 signifies bus error. 141 signifies memory exhaustion. 14
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+        process.exit(1);  // Exit the application with an error status code.
     }
-}
+};
 
-module.exports=connecttodb;
+module.exports = connecttodb;
